8dc345f89a4ae3d90baecd985eb4bf16587a9f7b
organic266-star <organic266@gmail.com>
2025-08-27 17:44:34 +0530

    Update VideoCallSocket.jsx

--- client/src/pages/components/socketio/VideoCallSocket.jsx
+++ client/src/pages/components/socketio/VideoCallSocket.jsx
@@ -4,7 +4,7 @@ let socket;
 
 const getSocket = () => {
     if (!socket) {
-        socket = io("https://chatsphere-anva.onrender.com")
+        socket = io("https://project-jv7y.onrender.com")
     }
     return socket;
 }
